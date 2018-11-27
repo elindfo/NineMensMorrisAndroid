1c173e9eb8f1e931eab6f7232ac2b271aa3aa63f
Erik Lindfors <elindfo@kth.se>
2018-11-27 02:34:55 +0100

    Add initial GameView code

--- app/src/main/java/com/example/erik/ninemensmorrisassignment/GameActivity.java
+++ app/src/main/java/com/example/erik/ninemensmorrisassignment/GameActivity.java
@@ -3,11 +3,16 @@ package com.example.erik.ninemensmorrisassignment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.erik.ninemensmorrisassignment.shape.GameView;
+
 public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gameView = findViewById(R.id.game_view);
     }
 }
--- app/src/main/java/com/example/erik/ninemensmorrisassignment/shape/GameView.java
+++ app/src/main/java/com/example/erik/ninemensmorrisassignment/shape/GameView.java
@@ -2,18 +2,97 @@ package com.example.erik.ninemensmorrisassignment.shape;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.example.erik.ninemensmorrisassignment.R;
+import com.example.erik.ninemensmorrisassignment.model.NineMensMorrisGame;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameView extends View {
 
+    private static final String TAG = "GameView";
+
+    private List<Drawable> gamePieces;
+    private Drawable currentPiece;
+    private Drawable background;
+    private NineMensMorrisGame model;
+    private Drawable blueCircle1;
+
+    private int width;
+    private int height;
+
+    private static final int CIRCLE_DIAMETER = 50;
+
     public GameView(Context context) {
         super(context);
+
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        model = NineMensMorrisGame.getInstance();
+        gamePieces = new ArrayList<>();
+        blueCircle1 = getResources().getDrawable(R.drawable.blue_circle);
+        background = getResources().getDrawable(R.drawable.morrisplayfield);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        this.width = w;
+        this.height = h;
+        blueCircle1.setBounds(
+                w / 2 - CIRCLE_DIAMETER / 2,
+                h / 2 - CIRCLE_DIAMETER / 2,
+                w / 2 + CIRCLE_DIAMETER / 2,
+                h / 2 + CIRCLE_DIAMETER / 2
+        );
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        canvas.drawColor(Color.TRANSPARENT);
-        
+        background.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        Log.d(TAG, "canvas width: " + canvas.getWidth() + ", canvas height: " + canvas.getHeight());
+        background.draw(canvas);
+        blueCircle1.draw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction()){
+            case MotionEvent.ACTION_MOVE:{
+                int x = (int)event.getX();
+                int y = (int)event.getY();
+                Log.d(TAG, "x: " + x + ", y: " + y);
+                blueCircle1.setBounds(
+                        x - CIRCLE_DIAMETER / 2,
+                        y - CIRCLE_DIAMETER / 2,
+                        x + CIRCLE_DIAMETER / 2,
+                        y + CIRCLE_DIAMETER / 2
+                );
+                break;
+            }
+            case MotionEvent.ACTION_DOWN:{
+                int x = (int)event.getX();
+                int y = (int)event.getY();
+                Log.d(TAG, "x: " + x + ", y: " + y);
+                break;
+            }
+            case MotionEvent.ACTION_UP:{
+
+                break;
+            }
+        }
+        invalidate();
+        return true;
     }
 }
