52ac2bb4e467b3b438f76dc88405d2bb669b0302
Unknown <joacimgr@kth.se>
2018-11-28 15:03:33 +0100

    Add menu to main and game -activity

--- app/src/main/java/com/example/erik/ninemensmorrisassignment/GameActivity.java
+++ app/src/main/java/com/example/erik/ninemensmorrisassignment/GameActivity.java
@@ -2,11 +2,17 @@ package com.example.erik.ninemensmorrisassignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.erik.ninemensmorrisassignment.model.NineMensMorrisGame;
 import com.example.erik.ninemensmorrisassignment.shape.GameView;
+
+import static com.example.erik.ninemensmorrisassignment.R.menu.menu;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -21,12 +27,26 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         gameView = findViewById(R.id.game_view);
-        resetButton = findViewById(R.id.reset_button);
-        resetButton.setOnClickListener((v) -> {
-            gameView.reset();
-        });
-
         currentPlayerText = findViewById(R.id.current_player_textview);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_refresh:
+                gameView.reset();
+                gameView.invalidate();
+                Toast.makeText(this, "New game started", Toast.LENGTH_LONG);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     public void updateText(String text){
